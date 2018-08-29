637ae713c23c9e38dfab7355110cf11116b589e5
Miles <milesasp@gmail.com>
2018-08-29 18:03:08 +0800

    加入準備要 demo 的空類別

---
+++ Lab-MVC/Repositories/InvoiceRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab_MVC.Repositories
+{
+    public class InvoiceRepository
+    {
+        
+    }
+}
