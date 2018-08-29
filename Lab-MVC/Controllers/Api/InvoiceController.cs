c2f15f9de0b88d861fca966b87b99d7ff809cca4
Miles <milesasp@gmail.com>
2018-08-29 18:49:45 +0800

    加入 Send Invoice WebAPI

---
+++ Lab-MVC/Controllers/Api/InvoiceController.cs
@@ -0,0 +1,30 @@
+﻿using Lab_MVC.Services;
+using System.Web.Http;
+
+namespace Lab_MVC.Controllers.Api
+{
+    public class InvoiceController : ApiController
+    {
+        private InvoiceService _invoiceService;
+
+        public InvoiceController()
+        {
+            _invoiceService = new InvoiceService();
+        }
+
+        [HttpPost]
+        public IHttpActionResult SendInvoice(string lastName, string theLastFourDigitalOfCreditCard)
+        {
+            bool isSent = _invoiceService.SendInvoice(lastName, theLastFourDigitalOfCreditCard);
+
+            if (isSent)
+            {
+                return Ok("Email 發送成功");
+            }
+            else
+            {
+                return BadRequest("Email 發送失敗");
+            }
+        }
+    }
+}
