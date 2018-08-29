e2546525b27ed3e410e76637cd6276c203b969a0
Miles <milesasp@gmail.com>
2018-08-29 18:17:43 +0800

    完成 PaymentTransactionRepository 範例 code

---
+++ Lab-MVC/Repositories/PaymentTransactionRepository.cs
@@ -0,0 +1,24 @@
+﻿using Lab_MVC.Models;
+using System.Linq;
+
+namespace Lab_MVC.Repositories
+{
+    public class PaymentTransactionRepository
+    {
+        private WebPayment _db;
+
+        public PaymentTransactionRepository()
+        {
+            _db = new WebPayment();
+        }
+
+        public PaymentTransaction GetLastPaymentTransactions(string lastName, string theLastFourDigitalOfCreditCard)
+        {
+            var paymentTransactions = _db.PaymentTransactions
+                .Where(x => x.LastName == lastName && x.LastFourNumberOfCard == theLastFourDigitalOfCreditCard)
+                .OrderByDescending(x => x.EntryDateTime);
+
+            return paymentTransactions.FirstOrDefault() ?? default(PaymentTransaction);
+        }
+    }
+}
