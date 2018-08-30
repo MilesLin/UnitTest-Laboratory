ea63f33a24fbfcd9640b7e7630961e10a36ad80e
Miles <milesasp@gmail.com>
2018-08-30 17:45:32 +0800

    完成用 NSubstitute 寫測試

--- Lab-MVC.Tests/Services/InvoiceServiceTests.cs
+++ Lab-MVC.Tests/Services/InvoiceServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using Lab_MVC.Interfaces.Repositories;
 using Lab_MVC.Interfaces.Services;
 using Lab_MVC.Models;
+using NSubstitute;
 using System;
 using System.Collections.Generic;
 using Xunit;
@@ -15,52 +16,34 @@ namespace Lab_MVC.Services.Tests
             // Arrange
             string lastName = "Lin";
             string theLastFourDigital = "1234";
-            var fakePaymentTransactionRepository = new FakePaymentTransactionRepository();
-            var fakePayPalService = new FakePayPalService();
+            string expectedMerchantPNRef = "Ref567";
 
-            var sut = new InvoiceService(fakePaymentTransactionRepository, fakePayPalService);
+            List<PaymentTransaction> paymentTransaction = new List<PaymentTransaction>()
+            {
+                new PaymentTransaction{ MerchantPNRef = "Ref123", EntryDateTime = new DateTime(2018, 8, 29), LastName = "Lin", LastFourNumberOfCard = "1234" },
+                new PaymentTransaction{ MerchantPNRef = expectedMerchantPNRef, EntryDateTime = new DateTime(2018, 8, 30), LastName = "Lin", LastFourNumberOfCard = "1234" }
+            };
+
+            var paymentTransactionRepository = Substitute.For<IPaymentTransactionRepository>();
+            paymentTransactionRepository
+                .GetPaymentTransactions(Arg.Is<string>(x => x == "Lin"), Arg.Is<string>(x => x == "1234"))
+                .Returns(paymentTransaction);
+
+            var payPalService = Substitute.For<IPayPalService>();
+            payPalService
+                .SendInvoice(Arg.Is<string>(x => x == expectedMerchantPNRef))
+                .Returns(true);
+
+            var sut = new InvoiceService(paymentTransactionRepository, payPalService);
 
             // Act
             var result = sut.SendInvoice(lastName, theLastFourDigital);
 
             // Assert
+            paymentTransactionRepository.Received(1)
+                .GetPaymentTransactions(Arg.Is<string>(x => x == "Lin"), Arg.Is<string>(x => x == "1234"));
+            payPalService.Received(1).SendInvoice(Arg.Is<string>(x => x == expectedMerchantPNRef));
             Assert.True(result);
-        }
-    }
-
-    public class FakePaymentTransactionRepository : IPaymentTransactionRepository
-    {
-        public IEnumerable<PaymentTransaction> GetPaymentTransactions(
-            string lastName,
-            string theLastFourDigitalOfCreditCard)
-        {
-            if (lastName != "Lin")
-            {
-                throw new System.Exception("傳入 lastName 參數錯誤");
-            }
-
-            if (theLastFourDigitalOfCreditCard != "1234")
-            {
-                throw new System.Exception("傳入 theLastFourDigitalOfCreditCard 參數錯誤");
-            }
-
-            yield return new PaymentTransaction
-            { MerchantPNRef = "Ref123", EntryDateTime = new DateTime(2018, 8, 29), LastName = "Lin", LastFourNumberOfCard = "1234" };
-            yield return new PaymentTransaction
-            { MerchantPNRef = "Ref567", EntryDateTime = new DateTime(2018, 8, 30), LastName = "Lin", LastFourNumberOfCard = "1234" };
-        }
-    }
-
-    public class FakePayPalService : IPayPalService
-    {
-        public bool SendInvoice(string merchantPNRef)
-        {
-            if (merchantPNRef != "Ref567")
-            {
-                throw new System.Exception("傳入 merchantPNRef 參數錯誤");
-            }
-
-            return true;
         }
     }
 }
