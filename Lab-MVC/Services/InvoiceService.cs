763c5063cdf9f1007647a15734d7fd4853c46c81
Miles <milesasp@gmail.com>
2018-08-30 10:23:53 +0800

    加入註解

--- Lab-MVC/Services/InvoiceService.cs
+++ Lab-MVC/Services/InvoiceService.cs
@@ -16,14 +16,19 @@ namespace Lab_MVC.Services
 
         public bool SendInvoice(string lastName, string theLastFourDigitalOfCreditCard)
         {
+            // 從資料庫取的「交易紀錄」
             var paymentTransactions = this._paymentTransactionRepository.GetPaymentTransactions(lastName, theLastFourDigitalOfCreditCard);
 
+            // 取得最後一筆交易紀錄
             var paymentTransaction = paymentTransactions.OrderByDescending(x => x.EntryDateTime).FirstOrDefault();
 
+            // 從交易紀錄取得 Reference Number
             string merchantPNRef = paymentTransaction.MerchantPNRef;
 
+            // 呼叫 PayPal API，寄送發票
             bool isSent = this._payPalService.SendInvoice(merchantPNRef);
 
+            // 回傳是否寄送成功
             return isSent;
         }
     }
