487db9eaa91a16d02ae4b6275882d9171c751200
Miles <milesasp@gmail.com>
2018-08-30 10:35:26 +0800

    修正註解錯字

--- Lab-MVC/Services/InvoiceService.cs
+++ Lab-MVC/Services/InvoiceService.cs
@@ -16,11 +16,14 @@ namespace Lab_MVC.Services
 
         public bool SendInvoice(string lastName, string theLastFourDigitalOfCreditCard)
         {
-            // 從資料庫取的「交易紀錄」
-            var paymentTransactions = this._paymentTransactionRepository.GetPaymentTransactions(lastName, theLastFourDigitalOfCreditCard);
+            // 根據「姓氏」與「信用卡後四碼」從資料庫取得「交易紀錄」
+            var paymentTransactions = this._paymentTransactionRepository
+                .GetPaymentTransactions(lastName, theLastFourDigitalOfCreditCard);
 
             // 取得最後一筆交易紀錄
-            var paymentTransaction = paymentTransactions.OrderByDescending(x => x.EntryDateTime).FirstOrDefault();
+            var paymentTransaction = paymentTransactions
+                .OrderByDescending(x => x.EntryDateTime)
+                .FirstOrDefault();
 
             // 從交易紀錄取得 Reference Number
             string merchantPNRef = paymentTransaction.MerchantPNRef;
