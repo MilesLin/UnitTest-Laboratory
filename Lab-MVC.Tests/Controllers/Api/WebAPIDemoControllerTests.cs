1191537b9865bed44ba868e9e1f759d33463a768
Miles <milesasp@gmail.com>
2018-08-31 12:14:34 +0800

    完成 Get OK 測試案例

---
+++ Lab-MVC.Tests/Controllers/Api/WebAPIDemoControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+using Lab_MVC.Controllers.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+using Lab_MVC.Models;
+
+namespace Lab_MVC.Controllers.Api.Tests
+{
+    public class WebAPIDemoControllerTests
+    {
+        [Fact()]
+        public void GetOkTest()
+        {
+            // Arrange
+
+            var sut = new WebAPIDemoController();
+
+            // Act
+            var result = sut.GetOk() as OkNegotiatedContentResult<Train>;
+
+            var a = result.Content;
+
+            // Assert
+            //Assert.Equal(expected, result);
+            //expected.ToExpectedObject().ShouldMatch(result);
+        }
+
+        [Fact()]
+        public void GetOkWithAnonymousTest()
+        {            
+            // Arrange
+            var sut = new WebAPIDemoController();
+
+            // Act
+            dynamic result = sut.GetOkWithAnonymous();
+
+            var a = result.Content;
+
+            // Assert
+            //Assert.Equal(expected, result);
+            //expected.ToExpectedObject().ShouldMatch(result);
+        }
+    }
+}
---
+++ Lab-MVC/Controllers/Api/WebAPIDemoController.cs
@@ -0,0 +1,28 @@
+﻿using Lab_MVC.Models;
+using System.Web.Http;
+
+namespace Lab_MVC.Controllers.Api
+{
+    public class WebAPIDemoController : ApiController
+    {
+        public IHttpActionResult GetOk()
+        {
+            var train = new Train()
+            {
+                TrainId = 1
+            };
+            return Ok(train);
+        }
+
+        public IHttpActionResult GetOkWithAnonymous()
+        {
+            var obj = new
+            {
+                Id = 1,
+                Name = "Miles"
+            };
+
+            return Ok(obj);
+        }
+    }
+}
