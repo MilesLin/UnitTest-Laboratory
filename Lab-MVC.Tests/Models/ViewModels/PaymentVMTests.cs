94882f77b13f67942746e0178723780455ba31c4
Miles <milesasp@gmail.com>
2018-08-31 17:25:42 +0800

    Model 的測試

---
+++ Lab-MVC.Tests/Models/ViewModels/PaymentVMTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Xunit;
+
+namespace Lab_MVC.Models.ViewModels.Tests
+{
+    public class PaymentVMTests
+    {
+        [Fact()]
+        public void ValidateTest()
+        {
+            // Arrange
+            var model = new PaymentVM()
+            {
+                Price = 10
+            };
+
+            // Act
+            var validationResults = new List<ValidationResult>();
+            var result = Validator.TryValidateObject(model, new ValidationContext(model), validationResults, validateAllProperties: true);
+
+            //validateAllProperties = true 會驗證 Range，反之不會。
+            //https://stackoverflow.com/questions/5636975/validator-validateobject-with-validateallproperties-to-true-stop-at-first-erro/5637762
+
+            //attribute 都通過，才會驗證 Validate 的內容，否則不會驗證 Valiate 的邏輯。
+
+            // Assert
+            Assert.False(result);
+            Assert.Equal("CreditCard", validationResults[0].MemberNames.ElementAt(0));
+            Assert.Equal("The CreditCard field is required.", validationResults[0].ErrorMessage);
+        }
+    }
+}
---
+++ Lab-MVC/Models/ViewModels/PaymentVM.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab_MVC.Models.ViewModels
+{
+    public class PaymentVM : IValidatableObject
+    {
+        [Range(0, 10)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string CreditCard { get; set; }
+
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Price <= 0)
+            {
+                yield return new ValidationResult("金額不能小於0元", new[] { "Price" });
+            }
+
+            if (string.IsNullOrEmpty(Address1) && string.IsNullOrEmpty(Address2))
+            {
+                yield return new ValidationResult("請輸入地址", new[] { "Address1", "Address2" });
+            }
+        }
+    }
+}
