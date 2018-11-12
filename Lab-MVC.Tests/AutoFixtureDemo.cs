136972ec2421143146058ab5fdfa40c121c893ec
Miles <milesasp@gmail.com>
2018-11-12 11:19:15 +0800

    comparer 範例

--- Lab-MVC.Tests/AutoFixtureDemo.cs
+++ Lab-MVC.Tests/AutoFixtureDemo.cs
@@ -21,6 +21,22 @@ namespace Lab_MVC.Tests
                 .With(x => x.Name, "Miles")
                 .Create();
         }
+
+        [Theory]
+        [InlineData(1,2,false)]
+        [InlineData(2,1,true)]
+        [InlineData(5,5,false)]
+        public void GreaterToTest(int number1, int number2, bool expected)
+        {
+            // Arrange
+            var sut = new Comparer();
+
+            // Act
+            var actual = sut.GreaterTo(number1, number2);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
     }
 
     public class Employee
@@ -28,5 +44,13 @@ namespace Lab_MVC.Tests
         public int Id { get; set; }
 
         public string Name { get; set; }
+    }
+
+    public class Comparer
+    {
+        public bool GreaterTo(int a, int b)
+        {
+            return a > b;
+        }
     }
 }
